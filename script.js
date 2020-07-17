ca12ee3a8fbcebb92162e18b0877f1ad2654fb64
Giga <giga@fakeemail.com>
2020-07-17 12:15:22 +0200

    giving a different class of box to each div via js

---
+++ script.js
@@ -0,0 +1,8 @@
+//giving a different class to each div with class = "box"
+const arr = "1,2,3,4,5,6,7,8,9,10".split(",");
+let i = 0;
+document.querySelectorAll(".box").forEach((div => {
+    div.classList.add(`box${arr[i]}`);
+    i++;  
+}));
+
